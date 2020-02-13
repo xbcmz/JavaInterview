4cd0c97a6c3ebb8c559fab3e38d14defdcb87aa6
Xbcmz <1570435771@qq.com>
2020-02-14 00:40:00 +0800

    java高并发

---
+++ optimize/AtomicInterArrayDemo.java
@@ -0,0 +1,31 @@
+package com.java.mr.optimize;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+public class AtomicInterArrayDemo {
+    static AtomicIntegerArray array = new AtomicIntegerArray(10);
+
+    public static class AddThread implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                array.getAndIncrement(i % array.length());
+            }
+        }
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(new AddThread());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            threads[i].start();
+        }
+        for (int i = 0; i < 10; i++) {
+            threads[i].join();
+            System.out.println(array);
+        }
+    }
+}
---
+++ optimize/ParseDateThreadLocalDemo.java
@@ -0,0 +1,39 @@
+package com.java.mr.optimize;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ParseDateThreadLocalDemo {
+    private static ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>();
+
+    public static class ParseDate implements Runnable {
+        int i = 0;
+
+        public ParseDate(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (threadLocal.get() == null) {
+                    threadLocal.set(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+                }
+                Date date = threadLocal.get().parse("20120-02-13 12:33:" + i % 60);
+                System.out.println(i + ":" + date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(new ParseDate(i));
+        }
+    }
+}
